7010fcaab0e0a45f0f3a1da4a5b477e094c97c1a
Prasundas99 <prasundas115@gmail.com>
2021-03-28 22:53:19 +0530

    Finalising project

--- src/API.tsx
+++ src/API.tsx
@@ -1,12 +1,27 @@
-function API() {
-    return (
-        <div>
-            
-        </div>
-    )
+import { shuffleArray } from './Utils';
+
+export type Question = {
+  category: string;
+  correct_answer: string;
+  difficulty: string;
+  incorrect_answers: string[];
+  question: string;
+  type: string;
+};
+
+export enum Difficulty {
+  EASY = "easy",
+  MEDIUM = "medium",
+  HARD = "hard",
 }
 
-export default API
+export type QuestionsState = Question & { answers: string[] };
 
-
-//Api = https://opentdb.com/api.php?amount=10&type=multiple;
+export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty): Promise<QuestionsState[]> => {
+  const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
+  const data = await (await fetch(endpoint)).json();
+  return data.results.map((question: Question) => ({
+    ...question,
+    answers: shuffleArray([...question.incorrect_answers, question.correct_answer])
+  }))
+};
